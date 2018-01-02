ee9f09a866b9a9b49a9adcf21bda31b8e0ce252b
aadhilrf <aadhilkh2010@gmail.com>
2018-01-02 22:22:07 +0530

    comments

--- script.sh
+++ script.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
+set -x
+
+#Download JDK in home/Downloads/tmp and extract it
 cd $HOME/Downloads && mkdir tmp && cd tmp
 filename="jdk-9.0.1_linux-x64_bin.tar.gz"
 version="jdk-9.0.1"
 wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/9.0.1+11/$filename
 tar -zxvf $filename
+
+#Install in /usr/local/java
 sudo mkdir /usr/local/java
 sudo mv ./$version /usr/local/java
+
+#set the Environmental variables and update-alternatives
 sudo echo -e "JAVA_HOME=/usr/local/java/$version\nJRE_HOME=\$JAVA_HOME/jre\nPATH=\$PATH:\$JAVA_HOME/bin:\$JRE_HOME/bin\nexport JAVA_HOME\nexport JRE_HOME\nexport PATH" >> /etc/profile
 sudo update-alternatives --install "/usr/bin/java" "java" "/usr/local/java/$version/jre/bin/java" 1
 sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/local/java/$version/bin/javac" 1
@@ -14,6 +21,7 @@ sudo update-alternatives --set java /usr/local/java/$version/jre/bin/java
 sudo update-alternatives --set javac /usr/local/java/$version/bin/javac
 sudo update-alternatives --set javaws /usr/local/java/$version/bin/javaws
 source /etc/profile
+
+#Delete the tmp folder
 cd $HOME/Downloads && rm -r tmp
-echo "installation successfull"
 
